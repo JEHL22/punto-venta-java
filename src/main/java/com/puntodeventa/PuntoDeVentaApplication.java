cbe1fb4b4b0745e742bcedb8ec1e064098ff4a3a
Julio Huaman <julio.e.huaman@hotmail.com>
2025-01-09 18:21:32 -0500

    Creacion del proyecto con spring initializr y clase principal

---
+++ src/main/java/com/puntodeventa/PuntoDeVentaApplication.java
@@ -0,0 +1,14 @@
+package com.puntodeventa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuntoDeVentaApplication {
+
+    /*metodo principal que ejecuta la aplicación Spring Boot*/
+    public static void main(String[] args) {
+        SpringApplication.run(PuntoDeVentaApplication.class, args);
+    }
+
+}
